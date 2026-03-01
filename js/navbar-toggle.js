99ad2283413d1c12d552b2b529b162a518630003
cn-guy <cindynguyen7544@gmail.com>
2026-02-28 18:21:18 -0800

    Adding blur feature upon navbar expansion

--- js/navbar-toggle.js
+++ js/navbar-toggle.js
@@ -3,4 +3,7 @@ const navButton = document.getElementById('menu-toggle');
 navButton.addEventListener("click", () => {
     const rightNavItems = document.querySelector(".right-nav");
     rightNavItems.classList.toggle("active");
+
+    const activeBlur = document.querySelector(".blur")
+    activeBlur.classList.toggle("active")
 })
