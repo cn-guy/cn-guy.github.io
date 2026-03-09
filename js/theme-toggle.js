2e9633e5012f4f31ccf8cef0701ac7ad9ec4b86f
cn-guy <cindynguyen7544@gmail.com>
2026-03-09 12:29:28 -0700

    Implementation for Contact Form Complete, Begin Styling

--- js/theme-toggle.js
+++ js/theme-toggle.js
@@ -4,8 +4,8 @@ const button = document.getElementById("theme-toggle");
 
 button.addEventListener("click", () => {
     //Determine which theme to change to
-    const elem = document.querySelector("html")
-    currentTheme = elem.getAttribute("theme")
+    const elem = document.querySelector("html");
+    currentTheme = elem.getAttribute("theme");
 
     const newTheme = currentTheme === "dark" ? "light" : "dark";
     
